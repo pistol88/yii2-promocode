c42d7aa0b74d4785e2116cd4e0c3e3a757124cad
pistol88 <pistol88@gmail.com>
2016-05-27 11:58:10 +0500

    fix desigh

--- widgets/views/enter_form.php
+++ widgets/views/enter_form.php
@@ -5,13 +5,20 @@ use yii\widgets\ActiveForm;
 ?>
 
 <div class="promo-code-enter">
+    
     <?php $form = ActiveForm::begin(['action' => ['/promocode/promo-code-use/enter']]); ?>
         <?php if(yii::$app->promocode->has()) { ?>
             <p class="promo-code-doscount">Ваша скидка: <?=yii::$app->promocode->get()->promocode->discount;?>%</p>
         <?php } ?>
-        <input type="hidden" name="<?= Yii::$app->request->csrfParam; ?>" value="<?= Yii::$app->request->csrfToken; ?>" />
-        <?=Html::input('text', 'promocode', yii::$app->promocode->getCode(), ['placeholder' => 'Промокод']) ?>
-        <?= Html::submitButton('Применить', ['class' => 'btn btn-success promo-code-enter-btn']) ?>
-        <?= Html::submitButton('Удалить', ['class' => 'btn btn-danger promo-code-clear-btn']) ?>
+        <div class="input-group">
+            <input type="hidden" name="<?= Yii::$app->request->csrfParam; ?>" value="<?= Yii::$app->request->csrfToken; ?>" />
+            <?=Html::input('text', 'promocode', yii::$app->promocode->getCode(), ['class' => 'form-control', 'placeholder' => 'Промокод']) ?>
+            <span class="input-group-btn">
+                <?= Html::submitButton('<i class="glyphicon glyphicon-ok"></i>', ['class' => 'btn btn-success promo-code-enter-btn']) ?>
+                <?= Html::submitButton('<i class="glyphicon glyphicon-remove"></i>', ['class' => 'btn btn-danger promo-code-clear-btn']) ?>
+            </span>
+        </div>
     <?php ActiveForm::end(); ?>
+    
 </div>
+
