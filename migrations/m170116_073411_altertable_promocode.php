8d5ba1f2d33d873282cedf8a16672f92d74d6849
usesgraphcrt <usesgraphcrt@gmail.com>
2017-01-17 18:23:15 +0500

    fix migrations

--- migrations/m170116_073411_altertable_promocode.php
+++ migrations/m170116_073411_altertable_promocode.php
@@ -14,14 +14,13 @@ class m170116_073411_altertable_promocode extends Migration
 
     public function safeUp()
     {
-        $tableOptions = 'ENGINE=InnoDB';
         $this->addColumn('{{%promocode}}','date_elapsed',$this->datetime()->null()->defaultValue(null));
         $this->addColumn('{{%promocode}}','amount',$this->integer(11)->null()->defaultValue(null));
     }
 
     public function safeDown()
     {
-        $this->dropColumn('{{%cashbox_operation}}', 'date_elapsed');
-        $this->dropColumn('{{%cashbox_operation}}', 'amount');
+        $this->dropColumn('{{%promocode}}', 'date_elapsed');
+        $this->dropColumn('{{%promocode}}', 'amount');
     }
 }
